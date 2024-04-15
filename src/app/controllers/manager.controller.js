5569b677e2c9730a9ef784d581bb92d04c445fdc
[marcellaamazonas] <[marcellaamazonas@gmail.com]>
2024-04-15 11:52:13 -0300

    Adicionar rota e controller de deletar usuario

--- src/app/controllers/manager.controller.js
+++ src/app/controllers/manager.controller.js
@@ -1,27 +1,38 @@
-import models from '../../infra/db/sequelize/models/index.js';
+import models from "../../infra/db/sequelize/models/index.js";
 // import { User } from '../../infra/db/sequelize/models/index.js';
 
-
 class ManagerController {
-    async listAllEmployees(req, res) {
-        try {
-            const employees = await models.User.findAll();
-            return res.status(200).json(employees);
-        } catch (error) {
-            return res.status(400).json({ error: error.message });
-        }
+  async listAllEmployees(req, res) {
+    try {
+      const employees = await models.User.findAll();
+      return res.status(200).json(employees);
+    } catch (error) {
+      return res.status(400).json({ error: error.message });
     }
-
-    async crateEmployee(req, res) {
-        const newEmployee = req.body;
-        try {
-          const createdEmployee = await models.User.create(newEmployee);
-          return res.status(201).json(createdEmployee);
-        } catch (error) {
-            return res.status(400).json({ error: error.message });
-      }
   }
 
-       
+  async crateEmployee(req, res) {
+    const newEmployee = req.body;
+    try {
+      const createdEmployee = await models.User.create(newEmployee);
+      return res.status(201).json(createdEmployee);
+    } catch (error) {
+      return res.status(400).json({ error: error.message });
+    }
   }
+
+  async deleteEmployee(req, res) {
+    const { id } = req.params;
+    try {
+      await models.User.destroy({
+        where: {
+          id: id,
+        },
+      });
+      return res.status(200).json({ message: `User with id ${id} deleted` });
+    } catch (error) {
+      return res.status(400).json({ error: error.message });
+    }
+  }
+}
 export default ManagerController;
--- src/app/routes/router.js
+++ src/app/routes/router.js
@@ -1,18 +1,26 @@
-import express from 'express';
+import express from "express";
 const router = express.Router();
-import ManagerController from '../controllers/manager.controller.js';
+import ManagerController from "../controllers/manager.controller.js";
 
-router.get('/healthcheck', (req, res) => {
-    res.json({ message: 'Health check: server online!' });
+router.get("/healthcheck", (req, res) => {
+  res.json({ message: "Health check: server online!" });
 });
 
 const managerController = new ManagerController();
 
 //rota do gestor para criar funcionário
-router.post('/api/manager/employee', (req,res) => managerController.crateEmployee(req,res))
+router.post("/api/manager/employee", (req, res) =>
+  managerController.crateEmployee(req, res)
+);
 
 // rota do gestor para listar todos os funcionários
-router.get('/api/manager/employee', (req,res ) => managerController.listAllEmployees(req,res));
+router.get("/api/manager/employee", (req, res) =>
+  managerController.listAllEmployees(req, res)
+);
 
+// rota do gestor para deletar funcionário
+router.delete("/api/manager/employee/:id", (req, res) =>
+  managerController.deleteEmployee(req, res)
+);
 
 export default router;
