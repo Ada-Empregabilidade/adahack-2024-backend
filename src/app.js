3d0bdd5b5b71bade6d8bbab8d0ec3e6d6f568729
[marcellaamazonas] <[marcellaamazonas@gmail.com]>
2024-04-16 11:09:12 -0300

    Adicionando a funcionalidade do Swagger

--- src/app.js
+++ src/app.js
@@ -1,18 +1,18 @@
 import express from "express";
 import cors from "cors";
 import router from "./app/routes/router.js";
-import authorization from './app/routes/middlewares/authorization.js';
+import authorization from "./app/routes/middlewares/authorization.js";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./swagger.json" assert { type: "json" };
 
 const app = express();
 
 app.use(cors({ origin: "http://localhost:8081" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 // NÃO COMENTAR A LINHA ABAIXO
 app.use("/api", authorization.authMiddleware, router);
-
-//testar a rota: localhost:3000/api/manager/employee/1
-// app.use('/api/manager/employee', router);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
