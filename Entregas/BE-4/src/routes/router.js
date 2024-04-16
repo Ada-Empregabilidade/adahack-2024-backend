9a78a919c6088a482ee2d6eacd777f28f168d530
Patrick Souza <147253362+PatrickPSouza@users.noreply.github.com>
2024-04-16 00:31:01 -0300

    feat: add full CRUD and routes for employee registration

--- Entregas/BE-4/src/routes/router.js
+++ Entregas/BE-4/src/routes/router.js
@@ -7,6 +7,33 @@ const service = require('../services/services')
 //  return res.status(200).json({ message: 'Tudo ok' });
 // })
 
+router.get('/api/cadastro/funcionarios', async (req, res) => {
+    try {
+        const employees = await service.getAllEmployees(db);
+        res.status(200).json(employees);
+    } catch (error) {
+        console.error("Erro ao obter funcionários:", error);
+        res.status(500).json({ error: "Erro interno de servidor" });
+    }
+});
+
+router.get('/api/cadastro/funcionarios/:id', async (req, res) => {
+    try {
+        const employeeId = req.params.id; 
+        const employee = await service.getEmployeeById(db, employeeId);
+        
+        if (employee) {
+            res.status(200).json(employee);
+        } else {
+            res.status(404).json({ error: "Funcionário não encontrado" });
+        }
+    } catch (error) {
+        console.error("Erro ao obter funcionário por ID:", error);
+        res.status(500).json({ error: "Erro interno de servidor" });
+    }
+});
+
+
 router.post('/api/cadastro/funcionarios', async (req, res) => {
     try {
         const employee = req.body;
@@ -17,7 +44,33 @@ router.post('/api/cadastro/funcionarios', async (req, res) => {
     }
 })
 
+router.put('/api/cadastro/funcionarios/:id', async (req, res) => {
+    try {
+        const employeeId = req.params.id; 
+        const employee = req.body;
+        const updatedEmployeeId = await service.updateEmployee(db, employeeId, employee);
+        res.status(200).json({ message: "Funcionário atualizado com sucesso", employeeId: updatedEmployeeId });
+    } catch (error) {
+        console.error("Erro ao atualizar funcionário:", error);
+        res.status(500).json({ error: "Erro interno de servidor" });
+    }
+});
 
+router.delete('/api/funcionarios/:id', async (req, res) => {
+    try {
+        const employeeId = req.params.id; 
+        const affectedRows = await service.deleteEmployee(db, employeeId);
+        
+        if (affectedRows > 0) {
+            res.status(200).json({ message: "Funcionário excluído com sucesso" });
+        } else {
+            res.status(404).json({ error: "Funcionário não encontrado" });
+        }
+    } catch (error) {
+        console.error("Erro ao excluir funcionário:", error);
+        res.status(500).json({ error: "Erro interno de servidor" });
+    }
+});
 
 
 module.exports = router;
