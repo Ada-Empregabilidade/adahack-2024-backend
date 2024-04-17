29798b90134771b0e71097f5b9fffeb9038512ed
Sownatta <wilsonn.rocha@outlook.com>
2024-04-17 07:20:17 -0300

    feat: add getall candidates service

--- Entregas/BE-4/src/services/candidateServices.js
+++ Entregas/BE-4/src/services/candidateServices.js
@@ -1,14 +1,41 @@
 const Candidate = require('../../config/db/models/candidate');
 
+
+async function updateCandidate(candidateId, candidateData) {
+    try {
+        const affectedRows = await Candidate.update(candidateData, {
+            where: { id: candidateId }
+        });
+        return affectedRows; 
+    } catch (error) {
+        throw new Error("Erro ao atualizar dados do candidatos: " + error.message);
+    }
+}
+
+async function deleteCandidate(candidateId) {
+    try {
+        const affectedRows = await Candidate.destroy({
+            where: { id: candidateId }
+        });
+        return affectedRows; 
+    } catch (error) {
+        throw new Error("Erro ao excluir candidatos: " + error.message);
+    }
+}
+
 async function getAllCandidates() {
     try {
         const candidate = await Candidate.findAll();
         return candidate;
     } catch (error) {
-        throw new Error("Erro ao obter funcionários do banco de dados: " + error.message);
+        throw new Error("Erro ao obter candidatos do banco de dados: " + error.message);
     }
 }
 
 module.exports = {
+
+    updateCandidate,
+    deleteCandidate,
     getAllCandidates,
+
 }
