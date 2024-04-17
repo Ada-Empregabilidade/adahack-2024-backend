52d04ca18fbe00c62ad4ef2c174832526baa8d01
ÍCARO VICTOR OLIVEIRA DOS SANTOS <victor.icarus@gmail.com>
2024-04-17 07:28:24 -0300

    feat: create put jobs service

---
+++ Entregas/BE-4/src/services/jobsService.js
@@ -0,0 +1,46 @@
+const Jobs = require('../../config/db/models/job'); 
+
+async function getAllJobs() {
+    try {
+        const jobs = await Jobs.findAll();
+        return jobs;
+    } catch (error) {
+        throw new Error("Erro ao obter vagas do banco de dados: " + error.message);
+    }
+}
+
+async function getJobsById(jobsId) {
+    try {
+        const jobs = await Jobs.findByPk(jobsId);
+        return jobs || null;
+    } catch (error) {
+        throw new Error("Erro ao obter vagas por ID do banco de dados: " + error.message);
+    }
+}
+
+async function registerJobs(jobsData) {
+    try {
+        const job = await Jobs.create(jobsData);
+        return job;       
+    } catch (error) {
+        throw new Error("Erro ao cadastrar o vagas: " + error.message);
+    }
+}
+
+async function updateJobs(jobsId, jobsData) {
+    try {
+        const affectedRows = await Jobs.update(jobsData, {
+            where: { id: jobsId }
+        });
+        return affectedRows; 
+    } catch (error) {
+        throw new Error("Erro ao atualizar dados do vagas: " + error.message);
+    }
+}
+
+module.exports = {
+    getAllJobs,
+    getJobsById,
+    registerJobs,
+    updateJobs
+};
