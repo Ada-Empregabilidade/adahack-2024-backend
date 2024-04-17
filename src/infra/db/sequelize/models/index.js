f04f5de6aac58667eb65ebb291aaabc23f1b9b2e
[marcellaamazonas] <[marcellaamazonas@gmail.com]>
2024-04-16 23:41:39 -0300

    Alterações .env.example

--- src/infra/db/sequelize/models/index.js
+++ src/infra/db/sequelize/models/index.js
@@ -1,31 +1,27 @@
-import * as dotenv from 'dotenv';
-import { DataTypes, Sequelize } from 'sequelize';
+import * as dotenv from "dotenv";
+import { DataTypes, Sequelize } from "sequelize";
 
 dotenv.config();
 const dbConfig = {
-    DB : process.env.DB,
-    USER : process.env.USER,
-    PASSWORD: process.env.PASSWORD,
-    HOST: process.env.HOST,
-    dialect: process.env.dialect
-}
+  DB: process.env.DATABASE,
+  USER: process.env.USERNAME,
+  PASSWORD: process.env.PASSWORD,
+  HOST: process.env.HOST,
+  dialect: process.env.dialect,
+};
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-    host: dbConfig.HOST,
-    dialect: dbConfig.dialect,
+  host: dbConfig.HOST,
+  dialect: dbConfig.dialect,
 });
 
-
 sequelize
-    .sync()
-    .then(() => console.log('Connection has been established successfully.'))
-    .catch(err =>
-        console.error('Unable to connect to the database:', err.message),
-    );
+  .sync()
+  .then(() => console.log("Connection has been established successfully."))
+  .catch((err) =>
+    console.error("Unable to connect to the database:", err.message)
+  );
 
-import UserModel from './user.model.js';
+import UserModel from "./user.model.js";
 const User = UserModel(sequelize, DataTypes);
 
-export {
-    sequelize,
-    User,
-};
+export { sequelize, User };
