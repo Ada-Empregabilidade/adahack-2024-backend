c9ca4b107161d44423ac02a40d0d6e99fafbaa3a
dev-gsilv <dev.gsilv@gmail.com>
2024-04-17 01:10:42 -0300

    fix: rewired model and repository

--- src/app/services/EmployeeService.js
+++ src/app/services/EmployeeService.js
@@ -1,11 +1,11 @@
-import { employeeRepository } from "../../infra/db/sequelize/repositories/EmployeeRepository.js";
+import { employeeRepository } from '../../infra/db/sequelize/repositories/EmployeeRepository.js';
 
 class EmployeeService {
     async execute(data) {
-        const update = await employeeRepository.registrationUpdate(data)
-
-        if(!update) throw new Error({message: "User not found"})
+        const update = await employeeRepository.registrationUpdate(data);
+        console.log('update', update[0]);
+        if (update[0] == 0) throw new Error('User not found');
     }
 }
 
-export const employeeService = new EmployeeService()
+export const employeeService = new EmployeeService();
