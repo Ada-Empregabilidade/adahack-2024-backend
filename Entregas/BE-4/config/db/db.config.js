0455df219866ad00a3f5c81586f2f04fcea7ffbf
adailtonasp <adailton-hag@hotmail.com>
2024-04-17 05:41:07 -0300

    feat/docker:create docker configuration files

--- Entregas/BE-4/config/db/db.config.js
+++ Entregas/BE-4/config/db/db.config.js
@@ -6,5 +6,6 @@ module.exports = {
     HOST: process.env.DB_HOST,
     USER: process.env.DB_USER,
     PASSWORD: process.env.DB_PASSWORD,
-    DB: process.env.DB_NAME
+    DB: process.env.DB_NAME,
+    PORT: process.env.DB_PORT
 };
